55068ffb0b79e2cffc00b3b3dd74486cdc878914
Alexandru Dimitriu <alexandru.dimitriu@gripsware.de>
2022-04-20 09:42:29 +0300

    Removed old iOS architectures

--- src/ios/makeUniversalLib.sh
+++ src/ios/makeUniversalLib.sh
@@ -2,6 +2,6 @@
 
 cd libs
 
-libtool -static i386/libchilkatIos-i386.a x86_64/libchilkatIos-x86_64.a armv7s/libchilkatIos-armv7s.a armv7/libchilkatIos-armv7.a arm64/libchilkatIos-arm64.a armv6/libchilkatIos-armv6.a -o libchilkatIos.a
+libtool -static x86_64/libchilkatIos-x86_64.a arm64/libchilkatIos-arm64.a -o libchilkatIos.a
 
 cd ..
